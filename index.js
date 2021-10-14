623f11a678b9053857b7e77bb1836fcc0a74370e
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2021-10-14 10:58:34 +0530

    java script Dom project

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const  saveChanges = () => {
     const taskData = {
       id: `${Date.now()}`, // unique number for id (it create a unique number for each and every mili second )
-      imageUrl:document.getElementById("imageurl").value,
-      taskTitle:document.getElementById("tasktitle").value,
-      tasktype:document.getElementById("tasktype").value,
-      taskDescription:document.getElementById("taskdescriptiom").value,
+      imageUrl: document.getElementById("imageurl").value,
+      taskTitle: document.getElementById("tasktitle").value,
+      taskType: document.getElementById("tasktype").value,
+      taskDescription: document.getElementById("taskdescriptiom").value,
     };
     console.log(taskData);
 };
