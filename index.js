02751b1717a688d9287148855118a59041ac162c
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2021-10-18 16:30:20 +0530

    local storage

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const taskContainer = document.querySelector(".task__container");
 
+globalStore=[];
+
   const generateNewCard = (taskData) => `<div class="col-md-6 col-lg-4"  id=${taskData.id} >
   <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
@@ -31,7 +33,72 @@ const  saveChanges = () => {
     
 
      taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+     
+     
+     globalStore.push(taskData);
+
+     localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+     // this this add it is usde to add the value to the local storage (set item)  it is a interface
 
 };
+
+
+
+
+//issues
+
+
+//page refresh will cause the data to  be deleted  --> local storage  --> 5MB
+// local storage is also known as API --> API  -->  Application Programming Interface
+
+
+// local storage --> Application 
+// Access the application via --> programming
+// Interface as a middle man
+//local storage --> with some method (Interface)       eg :- ( +,-,*,/ )  --> java script 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//features
+//delete the card
+// edit the card
+//open the card
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
