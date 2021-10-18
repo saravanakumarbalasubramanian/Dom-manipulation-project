c21c9d06c16c6f40e6971edc6532506b8b644ec8
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2021-10-18 12:45:06 +0530

    html tasky

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+const taskContainer = document.querySelector(".task__container");
+
+  const generateNewCard = (taskData) => `<div class="col-md-6 col-lg-4"  id=${taskData.id} >
+  <div class="card">
+    <div class="card-header d-flex justify-content-end gap-2">
+      <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+      <button type="button" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></button>
+    </div>
+    <img src=${taskData.imageUrl} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">${taskData.taskTitle}</h5>
+      <p class="card-text">${taskData.taskDescription}</p>
+      <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+     </div>
+     <div class="card-footer ">
+      <button type="button" class="btn btn-outline-primary float-end">open task</button>
+    </div>
+</div>
+`;
+
+
 const  saveChanges = () => {
     const taskData = {
       id: `${Date.now()}`, // unique number for id (it create a unique number for each and every mili second )
       imageUrl: document.getElementById("imageurl").value,
       taskTitle: document.getElementById("tasktitle").value,
       taskType: document.getElementById("tasktype").value,
-      taskDescription: document.getElementById("taskdescriptiom").value,
+      taskDescription: document.getElementById("taskdescription").value,
     };
-    console.log(taskData);
+    
+    
+
+     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
 };
+
+
